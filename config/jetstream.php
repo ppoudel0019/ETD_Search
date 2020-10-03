88240ba245f523f50cd00be208a1b88eef4b8edf
ppoudel0019 <ppoudel0019@users.noreply.github.com>
2020-10-03 02:02:57 -0400

    yay

--- config/jetstream.php
+++ config/jetstream.php
@@ -21,16 +21,12 @@ return [
     |--------------------------------------------------------------------------
     | Features
     |--------------------------------------------------------------------------
-    |
-    | Some of Jetstream's features are optional. You may disable the features
-    | by removing them from this array. You're free to only remove some of
-    | these features or you can even remove all of these if you need to.
-    |
-    */
+    | */
+   
 
     'features' => [
-        Features::profilePhotos(),
-        Features::api(),
+       // Features::profilePhotos(),
+        //Features::api(),
         // Features::teams(),
     ],
 
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -16,26 +16,7 @@
                     @livewire('profile.update-password-form')
                 </div>
             @endif
-
-            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <x-jet-section-border />
-
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
-                </div>
-            @endif
-
-            <x-jet-section-border />
-
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.logout-other-browser-sessions-form')
-            </div>
-
-            <x-jet-section-border />
-
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.delete-user-form')
-            </div>
+            
         </div>
     </div>
 </x-app-layout>
