8d9fd87b47bb1dca139d8ed570325044749363bf
ppoudel0019 <ppoudel0019@users.noreply.github.com>
2020-10-05 23:20:20 -0400

    Milestone 1 all requirements met

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -43,16 +43,7 @@
                 </x-jet-dropdown>
             </div>
 
-                
-  
-
-             
-
-           
-
-                      
-
-                       
+     
 
             <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
