7b68752be20806a9960241d439937222adc37087
ppoudel0019 <ppoudel0019@users.noreply.github.com>
2020-10-16 21:51:20 -0400

    Milestone-2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
+        <!-- Styles milestone-2-->
         <link rel="stylesheet"  href="{{ asset('/css/StyleSheet.css') }}">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
