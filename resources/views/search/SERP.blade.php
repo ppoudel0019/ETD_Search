ec7705a23a9496007758955debfe638db0421c16
ppoudel0019 <ppoudel0019@users.noreply.github.com>
2020-11-18 21:23:15 -0500

    Pagination & highlighting added

--- resources/views/search/SERP.blade.php
+++ resources/views/search/SERP.blade.php
@@ -1,178 +1,168 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Search Page</title>
-
-        <!-- Styles milestone-2-->
-        <link rel="stylesheet"  href="{{ asset('/css/StyleSheet.css') }}">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+@stack("styles")
+<script
+src="https://code.jquery.com/jquery-3.5.1.js"
+integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+crossorigin="anonymous"></script>
+@stack("scripts")
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.js"></script>
+<style>
 
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <style>
-body {margin:100px;padding:0}
+.content {
+  align:center;
+  margin-top: 100px;
+  margin-left: 600px;
+  margin-bottom: 20px;
+  max-width: 500px;
+  
+  font-size: 30px;
+}
+.tcontent{
+  
+  margin: 50px;
+  
+}
+mark{
+background: yellow;
+color: black;
+}
 
-       
-        </style>
-    </head>
+
+.text {
+  display: block;
+  width: 100ch;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.link{
+  color:blue;
+  font-weight: bold;
+
+}
+
+
+</style>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+
+
+
+<body>
     
-    <body class="antialiased">
-    
-        
-                <h1><form action="/search" method="POST"> 
+        <div class="content">
+                <form action="/search" method="POST"> 
                
                 @csrf
                     <input class="border border-red-500" type="text" name="q" >
-                    <input type="submit" value="Search" /> <i class="fa fa-search"></i></h1>
+                    <input type="submit" value="Search" /> 
                     </form> 
-                <form action="/advsearch" method="POST">
+                    <form action="{{URL::to('/advsearch')}}" method="POST">
                    {{ csrf_field() }}
-		            <div class="form-box">
-		           <button class ="search-btn" type="submit">Advanced Search</button>
-                </form>
-
-               
-                   
-
-
+                   <br>
+		            
+                   <h3><input type="submit" value="Advanced Search" /></button></h3>
+                     </form>
+</div>
+</div>
 
 
 <?php
 
-$client = Elasticsearch\ClientBuilder::create()->build();
+require '../vendor/autoload.php';
 
-$q=Request::get('q');
+$q=Request('q');
 
+$client = Elasticsearch\ClientBuilder::create()
+
+->build();
 $params = [
-  'index' => 'projectdata',
-  'body'  => [
-      'query'=> [
-         'bool' => [
-             'must' => [
-                 'multi_match' => [
-                 'query' => $q,
-                 'fields' => ['description_abstract','title','contributor_author','contributor_department','subject','type','handle','publisher','contributor_comitteemember']
-                 ]
-                ]
-             ]
-                 ],
-                 "highlight" => [
-                    "fields" => [
-                        "*" => [
-                            "pre_tags" => [
-                                "<mark>"
-                            ],
-                            "post_tags" => [
-                                "</mark>"],
-                                'require_field_match' => false
-                            ]
-                        ]
-                    ]
-
-                ],
-
-
-
-                 'size'=> 100
-         
-         
-
+     'index' => 'projectdata',
+      'body' => [
+'query'=> [
+'bool' => [
+'must' => [
+'multi_match' => [
+'query' => 
+$q ?? '',
+'fields' => ['title^3', 'degree_name', 'degree_level', 'description_abstract','publisher','type','contributor_department','identifier_uri','relation_haspart']
+]
+]
+]
+],
+ 'size'=> 100
+]
 ];
 
+$response = $client->search($params);
+$score = $response['hits']['hits'][0]['_score'];
+$total = $response['hits']['total']['value'];
+
+echo'<div class="tcontent">';
+
+echo "Searched for: ".$q;
+echo "<br>";
+echo "Total results: ".$total;
+echo "<br>";
+echo "<br>";
 
 
-          try{
-            $response = $client->search($params);
-            
-            $score = $response['hits']['hits'][0]['_score'];
-            $doc = $response['hits']['total']['value'];
-            echo "<br>";
-            echo "Searched for: ".$q;
-            echo "<br>";
-            echo "Total results: ".$doc;
+echo '
+
+<table class="table table-borderless" id="dt2">
+<thead>
+<th></th>
 
 
 
-
+</thead>
+<tbody>';
 
 foreach( $response['hits']['hits'] as $source){
+$ltitle= (isset($source['_source']['title'])? $source['_source']['title'] : "");
+$description= (isset($source['_source']['description_abstract'])? $source['_source']['description_abstract'] : "");
 
-$url=$source['_source']['identifier_sourceurl'];
-
-    echo "
-    
-    <div style='marigin: 10%'>
-    <div style='border: 0.1px  margin: 3%'>
-
-    <a href=".$source['_source']['identifier_sourceurl']."><p style='font-size:20px;'>".$source['_source']['title']."</p></b></a>
-    <form action='/mySearch' method='POST'>
-    <input type='submit' name='submit' value='Save'> 
-      </form> 
-           
-    <p class='type'>".$source['_source']['description_abstract']."</p> 
-    <p class='type'>".$source['_source']['contributor_author']."</p>
-    <p>".$source['_source']['publisher']."</p>
-    
-        <br>
-      </div>
-    </div>";
-    
-  } 
-
-
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-print_r($response);  
-
+$lauthor = (isset($source['_source']['contributor_author']) ? $source['_source']['contributor_author'] : "");
+$lpublisher= (isset($source['_source']['publisher']) ? $source['_source']['publisher'] : "");
+$sourceURL = (isset($source['_source']['identifier_uri']) ? $source['_source']['identifier_uri'] : ""); 
+echo "<tr><td><u><h5><a role='button' class='btn btn-link' href='".$sourceURL."' target='_blank'><div class='link'>$ltitle</div></u></a><br>$description<br>$lpublisher</td></tr>";
 }
 
-
-
-
-catch(Exception $e) {
-   
-}
-
-
-
-/*imp logic
-
-$data = app('es')->search($params);
-        // dump($data);die;
-        // dump($data['hits']['hits']);die;
-        foreach($data['hits']['hits'] as $k => $v){
-            // dump($data['hits']['hits'][$k]['_source']['long_title']);die;
-            $data['hits']['hits'][$k]['_source']['long_title'] = $v['highlight']['long_title'][0];
-        }
-        $realData = $data['hits']['hits'];
-        return view('web.member.search',[
-            'realData' => $realData
-*/
-
+echo "</tbody></table>";
+$doc = $response['hits']['hits'][0]['_source']['title'];
 ?>
 
 
+<script src="https://cdn.jsdelivr.net/mark.js/7.0.0/jquery.mark.min.js"></script>
+<script>
+$(document).ready( function () 
+{
+
+var table = $('#dt2').DataTable( { 
+"searching": false,
+"lengthMenu": [ 5,10, 25, 50, 75, 100 ],
+
+"initComplete": function( settings, json ) {
+$("body").unmark().mark("{{$q}}"); 
+    }
+});
 
 
-
-</body>
-</html>
+table.on( 'draw.dt', function () {
+$("body").unmark().mark("{{$q}}");
+} ); 
+}
+ );
+</script>
 
 
--- resources/views/search/advSERP.blade.php
+++ resources/views/search/advSERP.blade.php
@@ -1,44 +1,85 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Advanced Search</title>
-
-     <!-- Styles milestone-2-->
-     <link rel="stylesheet"  href="{{ asset('/css/StyleSheet.css') }}">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+@stack("styles")
+<script
+src="https://code.jquery.com/jquery-3.5.1.js"
+integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+crossorigin="anonymous"></script>
+@stack("scripts")
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.22/datatables.min.js"></script>
+<style>
 
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <style>
+.content {
+  align:center;
+  margin-top: 100px;
+  margin-left: 600px;
+  margin-bottom: 20px;
+  max-width: 500px;
+  
+  font-size: 30px;
+}
+.tcontent{
+  
+  margin: 50px;
+  
+}
+mark{
+background: yellow;
+color: black;
+}
 
-        
-body {margin:100px;padding:0}
 
-            
-        </style>
-    </head>
+.text {
+  display: block;
+  width: 100ch;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.link{
+  color:blue;
+  font-weight: bold;
+
+}
+
+
+</style>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+
+
+
+<body>
     
-    <body class="antialiased">
-    
-        
-                <h1><form action="/search" method="POST"> 
+        <div class="content">
+                <form action="/search" method="POST"> 
                
                 @csrf
                     <input class="border border-red-500" type="text" name="q" >
-                    <input type="submit" value="Search" /> <i class="fa fa-search"></i></h1>
+                    <input type="submit" value="Search" /> 
                     </form> 
                     <form action="{{URL::to('/advsearch')}}" method="POST">
                    {{ csrf_field() }}
-		            <div class="form-box">
-		           <button class ="search-btn" type="submit"> Advanced Search</button>
+                   <br>
+		            
+                   <h3><input type="submit" value="Advanced Search" /></button></h3>
                      </form>
+</div>
 </div>
 
 
 <?php
+
+require '../vendor/autoload.php';
 
 $client = Elasticsearch\ClientBuilder::create()->build();
 
@@ -71,48 +112,66 @@ $params = [
   
   ];
 
-try{
 
 $response = $client->search($params);
-
 $score = $response['hits']['hits'][0]['_score'];
+$total = $response['hits']['total']['value'];
 
-$doc = $response['hits']['total']['value'];
+echo'<div class="tcontent">';
 
-
-echo "<br>";
 echo "Searched for: ".$q;
 echo "<br>";
-echo "Total results: ".$doc;
+echo "Total results: ".$total;
+echo "<br>";
+echo "<br>";
 
+
+echo '
+
+<table class="table table-borderless" id="dt2">
+<thead>
+<th></th>
+
+
+
+</thead>
+<tbody>';
 
 foreach( $response['hits']['hits'] as $source){
-    echo "
-    <div style='marigin: 10%'>
-    <div style='border: 0.1px  margin: 3%'>
-    <br>
+$ltitle= (isset($source['_source']['title'])? $source['_source']['title'] : "");
+$description= (isset($source['_source']['description_abstract'])? $source['_source']['description_abstract'] : "");
 
-    <a href=".$source['_source']['identifier_sourceurl']."><p style='font-size:20px;'>".$source['_source']['title']."</p></b></a>
-    
-    <form action='/mySearch' method='POST'> 
-    <div class='form-box'>
-    <button class ='search-btn' type='submit'>Save</button>
-    <p class='type'>".$source['_source']['description_abstract']."</p> 
-    <p class='type'>".$source['_source']['contributor_author']."</p>
-    <p>".$source['_source']['publisher']."</p>
-  
-        
-      </div>
-    </div>";
-    
-}      
-    
-    
-
-
-}
-catch(Exception $e) {
-    // var_dump($e->getMessage());
+$lauthor = (isset($source['_source']['contributor_author']) ? $source['_source']['contributor_author'] : "");
+$lpublisher= (isset($source['_source']['publisher']) ? $source['_source']['publisher'] : "");
+$sourceURL = (isset($source['_source']['identifier_uri']) ? $source['_source']['identifier_uri'] : ""); 
+echo "<tr><td><u><h5><a role='button' class='btn btn-link' href='".$sourceURL."' target='_blank'><div class='link'>$ltitle</div></u></a><br>$description<br>$lpublisher</td></tr>";
 }
 
+echo "</tbody></table>";
+$doc = $response['hits']['hits'][0]['_source']['title'];
 ?>
+
+
+<script src="https://cdn.jsdelivr.net/mark.js/7.0.0/jquery.mark.min.js"></script>
+<script>
+$(document).ready( function () 
+{
+
+var table = $('#dt2').DataTable( { 
+"searching": false,
+"lengthMenu": [ 5,10, 25, 50, 75, 100 ],
+
+"initComplete": function( settings, json ) {
+$("body").unmark().mark("{{$q}}"); 
+    }
+});
+
+
+table.on( 'draw.dt', function () {
+$("body").unmark().mark("{{$q}}");
+} ); 
+}
+ );
+</script>
+
+
